7e7b51e82a7a3d5ad943b258a7828cd232eb4314
Maison GUILLEMAUT <servi@NASA.maison.guillemaut.net>
2020-03-21 05:16:06 +0100

    Fixed problem while displaying long titles on leftPanel videoTiles

--- youtube-library-app/src/components/videoTile.js
+++ youtube-library-app/src/components/videoTile.js
@@ -15,12 +15,39 @@ export default class VideoTile extends React.Component {
 
       this.state = {
         thumbClass : '',
+        title: '',
+        subtitle: '',
       }
+
       if(this.props.thumbsize === 'medium'){
         this.state.thumbClass = 'medium';
       }
       if(this.props.thumbsize === 'high'){
         this.state.thumbClass = 'high';
+      }
+
+      this.state.title = this.props.data.title;
+      this.state.subtitle = '';
+
+      if(this.props.data.title.length > 41){
+        this.state.title = '';
+        var arr = this.props.data.title.split(' ');
+        console.log(arr);
+        var i=0;
+        while ((this.state.title.length + arr[i].length) < 40) {
+          this.state.title = this.state.title.concat(arr[i]);
+          this.state.title = this.state.title.concat(' ');
+          console.log("Title = "+ this.state.title+" for i = "+i);
+            i++;
+        }
+        console.log("break for i = "+i);
+
+        for (let j = i; j < arr.length;j++) {
+          this.state.subtitle = this.state.subtitle.concat(arr[j]);  
+          this.state.subtitle = this.state.subtitle.concat(' ');
+          console.log("subtitle = "+ this.state.subtitle+" for j = "+j);
+       
+        }
       }
 
     }
@@ -31,11 +58,12 @@ export default class VideoTile extends React.Component {
                 </Popup>
                 */
 
+
         return(
 
             <GridListTile  key={this.props.data.id} cols={1} className={this.props.gridTileClass}>
                 <img src={this.props.data.thumbnails[this.props.thumbsize].url} className={this.state.thumbClass} />
-                <GridListTileBar title={this.props.data.title} />
+                <GridListTileBar title={this.state.title} subtitle={this.state.subtitle} />
  
 
             </GridListTile>
